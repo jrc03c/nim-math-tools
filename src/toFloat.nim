26c1f24732627322a17e66dee6c59d9782852bae
jrc03c <jrc03c@pm.me>
2024-04-20 16:55:28 -0500

    ...

--- src/toFloat.nim
+++ src/toFloat.nim
@@ -2,6 +2,19 @@ import std/sequtils
 import std/strutils
 import std/sugar
 
+proc toFloat*(x: bool): float =
+  if x:
+    return 1
+
+  return 0
+
+proc toFloat*(x: string): float =
+  try:
+    return parseFloat(x)
+
+  except:
+    return NaN
+
 proc toFloat*(x: seq[int]): seq[float] =
   return x.map(v => float(v))
 
@@ -13,8 +26,32 @@ proc toFloat*[T](x: seq[T]): seq[untyped] =
     when T is SomeNumber:
       return x.map(v => toFloat(v))
 
+    elif T is bool:
+      return x.map(v => toFloat(v))
+
     elif T is string:
-      return x.map(v => parseFloat(v))
+      var temp: seq[float] = @[]
+
+      for v in x:
+        try:
+          temp.add(parseFloat(v))
+
+        except:
+          temp.add(NaN)
+
+      return temp
 
     else:
       return x.map(v => NaN)
+
+proc float*(x: bool): float =
+  return toFloat(x)
+
+proc float*(x: string): float =
+  return toFloat(x)
+
+proc float*(x: seq[int]): seq[float] =
+  return toFloat(x)
+
+proc float*[T](x: seq[T]): seq[untyped] =
+  return toFloat(x)
