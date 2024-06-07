3e50c447e60a18d204ee40c17fa8359e18eafd9e
jrc03c <jrc03c@pm.me>
2024-06-06 21:35:11 -0500

    ...

---
+++ src/ndarray.nim
@@ -0,0 +1,28 @@
+template ndarray*(shape: openArray[int], T: typedesc): untyped =
+  when len(shape) == 0:
+    return newSeq[T]()
+
+  when len(shape) == 1:
+    var temp: seq[T] = @[]
+
+    for i in 0 .. shape[0] - 1:
+      temp.add(default(T))
+
+    temp
+
+  elif len(shape) == 2:
+    var temp: seq[seq[T]] = @[]
+
+    for i in 0 .. shape[0] - 1:
+      temp.add(ndarray([shape[1]], T))
+
+    temp
+
+  else:
+    let row = ndarray(shape[1..^1], T)
+    var temp: seq[typeof(row)] = @[]
+
+    for i in 0 .. shape[0] - 1:
+      temp.add(row)
+
+    temp
