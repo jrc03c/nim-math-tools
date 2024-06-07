cb49fb6309a9c1e0a19ffd7dec889fca9f0ca9e2
jrc03c <jrc03c@pm.me>
2024-06-07 07:00:05 -0500

    ...

--- src/chop.nim
+++ src/chop.nim
@@ -1,5 +1,11 @@
 import apply
 
+proc chop*(x: float, threshold: float = 0.0000000001): float =
+  if abs(x) < threshold:
+    return 0.0
+
+  return x
+
 proc chop*[T](x: seq[T], threshold: float = 0.0000000001): seq[untyped] =
   proc helper(v: float): float =
     if abs(v) < threshold:
