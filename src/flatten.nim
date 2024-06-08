374ca6b36e8673cf0f3a3334840c1a1090be60f7
jrc03c <jrc03c@pm.me>
2024-06-08 14:37:44 -0500

    ...

--- src/flatten.nim
+++ src/flatten.nim
@@ -1,17 +1,15 @@
-template flatten*[T](x: seq[T]): untyped =
+import innerType
+
+export innerType
+
+proc flatten*[T](x: seq[T]): seq[untyped] =
   when T is seq:
-    if len(x) > 0:
-      let firstRow = flatten(x[0])
-      var temp: seq[typeof(firstRow[0])] = @[]
-      temp.add(firstRow)
+    var temp: seq[innerType(x)] = @[]
 
-      for v in x[1..^1]:
-        temp.add(flatten(v))
+    for v in x:
+      temp.add(flatten(v))
 
-      temp
-
-    else:
-      newSeq[float]()
+    return temp
 
   else:
-    x
+    return x
