b0a49239de15d890e38afadf7dea83abe183d920
jrc03c <jrc03c@pm.me>
2024-06-08 15:03:35 -0500

    ...

--- src/count.nim
+++ src/count.nim
@@ -1,1 +1,20 @@
+import innerType
+import set
+import std/tables
 
+export innerType
+export set
+export tables
+
+proc count*[T](x: seq[T]): TableRef[untyped, int] =
+  type t = innerType(x)
+  let xset = set[T](x)
+  var table = newTable[t, int]()
+
+  for v in xset:
+    if not table.contains(v):
+      table[v] = 0
+
+    table[v] += 1
+
+  return table
