f231577196cbfc043d31f616b3ae6749e1df4c60
jrc03c <jrc03c@pm.me>
2024-06-15 09:46:24 -0500

    ...

--- src/abs.nim
+++ src/abs.nim
@@ -1,4 +1,20 @@
 import apply
+import innerType
+
+export apply
+export innerType
+
+proc abs*(x: bool): float =
+  if x:
+    return 1.0
+
+  return 0
 
 proc abs*[T](x: seq[T]): seq[untyped] =
-  return apply(x, proc(v: float): float = abs(v))
+  type t = innerType(x)
+
+  when t is bool:
+    return apply(x, proc(v: bool): float = abs(v))
+
+  else:
+    return apply(x, proc(v: t): t = abs(v))
--- src/apply.nim
+++ src/apply.nim
@@ -1,9 +1,9 @@
 import std/sequtils
 import std/sugar
 
-proc apply*[T, U](x: seq[T], fn: proc(v: U): U): seq[untyped] =
+template apply*[T, U, V](x: T, fn: proc(u: U): V): untyped =
   when T is seq:
-    return x.map(v => apply(v, fn))
+    x.map(v => apply(v, fn))
 
   else:
-    return x.map(v => fn(v))
+    fn(x)
