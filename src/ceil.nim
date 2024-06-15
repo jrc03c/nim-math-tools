c71a77cc67c655a9ab9342f0c9c1eff0f4178db4
jrc03c <jrc03c@pm.me>
2024-06-15 12:14:20 -0500

    ...

--- src/ceil.nim
+++ src/ceil.nim
@@ -1,4 +1,14 @@
 import apply
+import std/math
+
+export apply
+export math.ceil
+
+proc ceil*(x: bool): float =
+  if x:
+    return 1.0
+
+  return 0.0
 
 proc ceil*[T](x: seq[T]): seq[untyped] =
-  return apply(x, proc(v: float): float = ceil(v))
+  return apply(x, proc(v: float): float = math.ceil(v))
--- src/ceilTest.nim
+++ src/ceilTest.nim
@@ -1,14 +1,11 @@
 import ceil
 import operatorApprox
 
-assert ceil(234) == 234
-assert ceil(-234) == -234
 assert ceil(2.3) ~ 3.0
 assert ceil(-2.3) ~ -2.0
 assert ceil(true) ~ 1.0
 assert ceil(false) ~ 0.0
-assert ceil(@[2, 3, 4]) == @[2, 3, 4]
-assert ceil(@[-2, -3, -4]) == @[-2, -3, -4]
 assert ceil(@[2.3, 4.5, 6.7]) ~ @[3.0, 5.0, 7.0]
 assert ceil(@[-2.3, -4.5, -6.7]) ~ @[-2.0, -4.0, -6.0]
-assert ceil(@[@[1.2, -3.4], @[5.6, -7.8], @[9.10, -11.12]]) ~ @[@[2.0, -3.0], @[6.0, -7.0], @[10.0, -11.0]]
+assert ceil(@[@[1.2, -3.4], @[5.6, -7.8], @[9.10, -11.12]]) ~ @[@[2.0, -3.0], @[
+    6.0, -7.0], @[10.0, -11.0]]
