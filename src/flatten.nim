1b7d905c111785548697a914d38f87b8cc743b65
jrc03c <jrc03c@pm.me>
2024-06-16 09:10:58 -0500

    ...

--- src/flatten.nim
+++ src/flatten.nim
@@ -2,6 +2,10 @@ import innerType
 
 export innerType
 
+proc flatten*[T](x: T): seq[untyped] =
+  var temp: seq[T] = @[x]
+  return temp
+
 proc flatten*[T](x: seq[T]): seq[untyped] =
   when T is seq:
     var temp: seq[innerType(x)] = @[]
