c1326c4084fbd873a43cfecf2283a5375238d371
jrc03c <jrc03c@pm.me>
2024-06-16 08:38:47 -0500

    ...

--- src/diff.nim
+++ src/diff.nim
@@ -7,13 +7,19 @@ export set
 # NOTE: This function only finds the elements in `a` that are not in `b`; it
 # does NOT also find elements in `b` that are not in `a`!
 
-proc diff*[T](a: seq[T], b: seq[T]): seq[untyped] =
+proc diff*[T, U](a: seq[T], b: seq[U]): seq[untyped] =
   let aset = set[T](a)
-  let bset = set[T](b)
+  let bset = set[U](b)
   var temp: seq[innerType(a)] = @[]
 
   for v in aset:
     if not bset.contains(v):
       temp.add(v)
 
+  return temp
+
+proc diffBoth*[T, U](a: seq[T], b: seq[U]): seq[untyped] =
+  var temp: seq[innerType(a)] = @[]
+  temp.add(diff(a, b))
+  temp.add(diff(b, a))
   return temp
--- src/diffTest.nim
+++ src/diffTest.nim
@@ -5,6 +5,13 @@ assert diff(@[2, 3, 4], @[4, 5, 6]) == @[2, 3]
 
 assert sort(diff(@[@[@[5, 3, 1, 2, 4, 6]]], @[@[@[1, 3, 5, 7, 9]]])) == @[2, 4, 6]
 
-assert diff(@[2, 3, 4], @[2, 3, 4]) == @[]
-assert diff(@[2, 3, 4], @[]) == @[2, 3, 4]
+let emptyIntSeq: seq[int] = @[]
+assert diff(@[2, 3, 4], @[2, 3, 4]) == emptyIntSeq
+assert diff(@[2, 3, 4], emptyIntSeq) == @[2, 3, 4]
 assert diff(@["a", "b", "c"], @["a", "c"]) == @["b"]
+assert diff(@[@[@[1.2, 3.4, 5.6]]], @[1.2]) == @[3.4, 5.6]
+
+assert diffBoth(@[2, 3, 4], @[3, 4, 5]) == @[2, 5]
+
+assert diffBoth(@[@["foo", "bar"], @["hello", "world"]], @[@[@["hello",
+    "foo"]]]) == @["bar", "world"]
