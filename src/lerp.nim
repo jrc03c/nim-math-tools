d271fa5e9b223c6b9df199c827862f2a895f6665
jrc03c <jrc03c@pm.me>
2024-06-16 09:35:57 -0500

    ...

--- src/lerp.nim
+++ src/lerp.nim
@@ -1,2 +1,9 @@
+import operatorRangeSeq
+import std/sequtils
+import std/sugar
+
 proc lerp*(a: float, b: float, f: float): float =
   return f * (b - a) + a
+
+proc lerp*[T](a: seq[T], b: seq[T], f: float): seq[untyped] =
+  return (0 -> len(a) - 1).map(i => lerp(a[i], b[i], f))
