dc9f467ed72932fb2555895380ca179f6583f2ba
jrc03c <jrc03c@pm.me>
2024-06-16 09:07:37 -0500

    ...

---
+++ src/factorialTest.nim
@@ -0,0 +1,10 @@
+import factorial
+
+assert factorial(0) == 1
+assert factorial(1) == 1
+assert factorial(2) == 2
+assert factorial(3) == 6
+assert factorial(4) == 24
+assert factorial(5) == 120
+assert factorial(5.0) == 120.0
+assert factorial(5.9) == 120.0
