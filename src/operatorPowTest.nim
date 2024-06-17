4cbbe373e827c41a8759dc0606c1ee7b9d0ecd0f
jrc03c <jrc03c@pm.me>
2024-06-16 21:39:40 -0500

    ...

---
+++ src/operatorPowTest.nim
@@ -0,0 +1,9 @@
+import operatorApprox
+import operatorPow
+
+assert 2 ** 3 == 8
+assert 4 ** 3 == 64
+assert 2.0 ** 3.0 == 8.0
+assert 1.2 ** 3.4 ~ 1.858729692
+assert @[2, 3, 4] ** 3 == @[8, 27, 64]
+assert @[@[@[2, 3, 4]]] ** @[@[@[5, 6, 7]]] == @[@[@[32, 729, 16384]]]
