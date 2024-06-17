47b2c21e64024dfe46df9166eb8354388d4860d1
jrc03c <jrc03c@pm.me>
2024-06-16 21:44:47 -0500

    ...

---
+++ src/productTest.nim
@@ -0,0 +1,9 @@
+import operatorApprox
+import product
+
+assert product(@[0]) == 0
+assert product(@[5]) == 5
+assert product(@[-5]) == -5
+assert product(@[2, 3, 4]) == 24
+assert product(@[2.0, 3.0, 4.0]) == 24.0
+assert product(@[@[2.3, 4.5], @[6.7, 8.9]]) ~ 617.1705
