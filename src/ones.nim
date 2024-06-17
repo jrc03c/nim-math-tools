dcb1f654f106cfdbfc3190bc0ecc4e94463bb220
jrc03c <jrc03c@pm.me>
2024-06-16 21:29:35 -0500

    ...

--- src/ones.nim
+++ src/ones.nim
@@ -1,7 +1,11 @@
 import apply
 import zeros
 
+export apply
 export zeros
 
 template ones*(shape: seq[int]): seq[untyped] =
   apply(zeros(shape), proc(v: float): float = 1.0)
+
+template ones*(n: int): seq[untyped] =
+  ones(@[n])
