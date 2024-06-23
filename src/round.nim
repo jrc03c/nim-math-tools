15fcd109bff8d8c4e9df1e4b7d7c25cca65aa302
jrc03c <jrc03c@pm.me>
2024-06-23 18:10:42 -0500

    ...

--- src/round.nim
+++ src/round.nim
@@ -1,8 +1,8 @@
-from math import round
 import apply
+import std/math
 
 export apply
-export round
+export math.round
 
 proc round*[T](x: seq[T]): seq[untyped] =
   return apply(x, proc(v: float): float = round(v))
