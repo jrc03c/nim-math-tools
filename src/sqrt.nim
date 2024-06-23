6a974bd5b7e68b2d14483a4869df80305411770a
jrc03c <jrc03c@pm.me>
2024-06-23 18:51:08 -0500

    ...

--- src/sqrt.nim
+++ src/sqrt.nim
@@ -7,12 +7,6 @@ export math.sqrt
 proc sqrt*(x: int): float =
   return math.sqrt(float(x))
 
-proc sqrt*(x: bool): float =
-  if x:
-    return 1.0
-
-  return 0.0
-
 proc sqrt*[T](x: seq[T]): seq[untyped] =
   when T is seq:
     return x.map(v => sqrt(v))
--- src/sqrtTest.nim
+++ src/sqrtTest.nim
@@ -9,7 +9,3 @@ assert sqrt(@[2.3, 4.5, 6.7]) ~ @[sqrt(2.3), sqrt(4.5), sqrt(6.7)]
 
 assert sqrt(@[@[1.2, 3.4], @[5.6, 7.8], @[9.10, 11.12]]) ~ @[@[sqrt(1.2), sqrt(
     3.4)], @[sqrt(5.6), sqrt(7.8)], @[sqrt(9.10), sqrt(11.12)]]
-
-assert sqrt(true) == 1.0
-assert sqrt(false) == 0.0
-assert sqrt(@[@[true, false, true]]) == @[@[1.0, 0.0, 1.0]]
