43a02780a50804fd0b230d542c00bb369949aaf9
jrc03c <jrc03c@pm.me>
2024-07-11 18:39:00 -0500

    ...

--- src/operators.nim
+++ src/operators.nim
@@ -1,6 +1,8 @@
 include operator_add
 include operator_approx
 include operator_div
+include operator_mod
 include operator_mul
+include operator_pow
 include operator_range_seq
 include operator_sub
